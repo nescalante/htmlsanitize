510c683a18a5e07dcbe9a2f6b6dd7c9723d4d636
Nicolas Escalante <nlante@gmail.com>
2017-11-16 00:29:49 -0300

    add more test cases

--- test/lib.js
+++ test/lib.js
@@ -12,7 +12,8 @@ describe('sanitize', () => {
       '<div>foo</div>',
       '<a href="https://foo">bar</a>',
       '<h2>Header</h2>\nParagraph.',
-      '<table><thead><tr><th>head</th></tr></thead><tbody><tr><td>foo</td></tr></tbody></table>'
+      '<table><thead><tr><th>head</th></tr></thead><tbody><tr><td>foo</td></tr></tbody></table>',
+      '<table><tbody><tr><td>foo</td></tr></tbody></table>'
     ];
 
     testCases.forEach((testCase) => {
@@ -29,6 +30,10 @@ describe('sanitize', () => {
       [
         '<div onclick="alert(\'buh!\')">foo</div>',
         '<div>foo</div>'
+      ],
+      [
+        '<table><tr><td>foo</td></tr></table>',
+        '<table><tbody><tr><td>foo</td></tr></tbody></table>'
       ],
       [
         '"quotes"',
